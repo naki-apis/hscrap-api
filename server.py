c8349cebe60a7d26984ad0b51dbd5b2484cdb2a5
naki-apis <nakige.player@gmail.com>
2026-01-03 07:42:09 -0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+from scrap import NHentaiScraper
+
+class NHentaiAPI:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.scraper = NHentaiScraper()
+        self.setup_routes()
+    
+    def setup_routes(self):
+        @self.app.route('/')
+        def index():
+            return "API de scraping nhentai"
+        
+        @self.app.route('/snh/')
+        def snh_search():
+            query = request.args.get('q', '')
+            page = request.args.get('p', '1')
+            
+            if not query:
+                return jsonify({"error": "Parámetro 'q' requerido"}), 400
+            
+            try:
+                page_num = int(page)
+            except:
+                page_num = 1
+            
+            results = self.scraper.search(query, page_num)
+            return jsonify(results)
+    
+    def run(self, host='0.0.0.0', port=5000):
+        self.app.run(host=host, port=port, debug=False, use_reloader=False)
+
+if __name__ == '__main__':
+    api = NHentaiAPI()
+    api.run()
